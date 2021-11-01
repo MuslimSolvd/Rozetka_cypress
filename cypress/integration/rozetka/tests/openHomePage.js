bde30ea617f5edf2c9320790789de460f778056f
muslimbairamov <muslimbairamov@gmail.com>
2021-11-01 18:54:01 +0200

    Add Page Object HomePage to the test

--- cypress/integration/rozetka/tests/openHomePage.js
+++ cypress/integration/rozetka/tests/openHomePage.js
@@ -1,10 +1,15 @@
 /// <reference types="cypress" />
 
+import HomePage from '../pages/homePage'
+
 context('Window', () => {
+
+  const homePage = new HomePage()
+
   beforeEach(() => {
     cy.viewport(1920, 1080)
     cy.visit('https://rozetka.com.ua/')
-    
+    cy.location('protocol').should('eq', 'https:')
   })
 
   it('cy.window() - get the global window object', () => {
